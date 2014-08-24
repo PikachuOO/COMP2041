8d491b36f10b436de99d09280a27a5d0c70bd802
CallumHoward <callumh@cse.unsw.edu.au>
2014-08-24 18:21:05 +1000

    challenge exercise 1 for week04

--- week04/date_image-challenge.sh
+++ week04/date_image-challenge.sh
@@ -4,7 +4,14 @@ for file in *.jpg
 do
     # handle if there are no image files
     [ -e "$file" ] || continue
-    date=`date -r "$file" | egrep -o '[A-Z][a-z]{2} [0-9]+ [0-9]{2}:[0-9]{2}'`
+
+    # get original date
+    date=`date -r new04 +"%Y%m%d%H%M"`
+
+    # annotate image
     convert -gravity south -pointsize 36 -annotate 0 "$date" "$file" "$file"
+
+    # resotre original modification date
+    touch -t"$date" file
 done
 
