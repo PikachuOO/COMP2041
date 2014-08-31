118f96898994481f702d08b0c622f5cdb583a06d
CallumHoward <callumh@cse.unsw.edu.au>
2014-08-31 12:16:53 +1000

    gcd.py works

--- misc/gcd.py
+++ misc/gcd.py
@@ -4,5 +4,4 @@ import sys
 gcd = lambda x, y : x if y == 0 else gcd(y, x%y)
 
 assert( len(sys.argv) == 3 )
-print( gcd(sys.argv[1], sys.argv[2]) )
-
+print( gcd(long(sys.argv[1]), long(sys.argv[2])) )
