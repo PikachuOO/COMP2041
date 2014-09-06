7c0bf3ce27a2a34a5de1efaa086dd07be3a73a43
CallumHoward <callumh@cse.unsw.edu.au>
2014-09-06 21:40:25 +1000

    prereq fixed up newlines

--- week06/prereq.pl
+++ week06/prereq.pl
@@ -5,14 +5,11 @@ use strict;
 
 my $course = $ARGV[0] or die "Usage: $0 <course-name>\n";
 $course =~ /^([A-Z]{4}\d{4})$/ or die "Invalid course format: $course\n";
-my $nLFlag = 0;
 my $urlU = "http://www.handbook.unsw.edu.au/undergraduate/courses/2014/$1.html";
 my $urlP = "http://www.handbook.unsw.edu.au/postgraduate/courses/2014/$1.html";
 my $string = `wget -q -O- $urlU $urlP`;
 
 while ($string =~ s/(?<=Prerequisite:).*?([A-Z]{4}\d{4})// ) {             
-    if ($nLFlag) {print "\n";} else {$nLFlag = 1;}
-    print $1;
+    print $1,"\n";
 }
-print "\n";
 
