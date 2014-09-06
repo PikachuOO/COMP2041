b05d878ad499568555126bd99bb80378b36b085c
CallumHoward <callumh@cse.unsw.edu.au>
2014-09-06 23:41:31 +1000

    new version of prereq.pl, easier to read

--- week06/prereq.pl
+++ week06/prereq.pl
@@ -3,10 +3,20 @@
 use warnings;
 use strict;
 
+my $domain="www.handbook.unsw.edu.au";
+my $year=2014;
+
+my $found=0;
+my $content;
+
 my $course = $ARGV[0] or die "Usage: $0 <course-name>\n";
-$course =~ /^([A-Z]{4}\d{4})$/ or die "$0: Invalid course format: $course\n";
-my $urlU = "http://www.handbook.unsw.edu.au/undergraduate/courses/2014/$1.html";
-my $urlP = "http://www.handbook.unsw.edu.au/postgraduate/courses/2014/$1.html";
-my $string = (`wget -q -O- $urlU` or `wget -q -O- $urlP`) 
-    or die "$0: Course not found: $course\n";
-print uc $1, "\n" while ($string =~ s/(?<=equisite)[^<]*?(\b[A-Za-z]{4}\d{4}\b)//);
+$course =~ /^[A-Z]{4}\d{4}$/ or die "$0: Invalid course format: $course\n";
+
+foreach my $career ("undergraduate","postgraduate") {
+    my $url = "http://$domain/$career/courses/$year/$course.html";
+    $content .= `wget -q -O- $url`; 
+    $found += $?;
+}
+
+die "$0: Course not found: $course\n" if (!$found);
+print uc $1,"\n" while ($content =~ s/(?<=equisite)[^<]*?(\b[A-Za-z]{4}\d{4}\b)//);
