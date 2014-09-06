047479e0a9758414a905b58b26ce7f4d35ae8103
CallumHoward <callumh@cse.unsw.edu.au>
2014-09-06 23:10:16 +1000

    prereq fixed for coreq and excluded

--- week06/prereq.pl
+++ week06/prereq.pl
@@ -4,9 +4,9 @@ use warnings;
 use strict;
 
 my $course = $ARGV[0] or die "Usage: $0 <course-name>\n";
-$course =~ /^([A-Z]{4}\d{4})$/ or die "Invalid course format: $course\n";
+$course =~ /^([A-Z]{4}\d{4})$/ or die "$0: Invalid course format: $course\n";
 my $urlU = "http://www.handbook.unsw.edu.au/undergraduate/courses/2014/$1.html";
 my $urlP = "http://www.handbook.unsw.edu.au/postgraduate/courses/2014/$1.html";
-my $string = `wget -q -O- $urlU $urlP`;
-
-print uc $1, "\n" while ($string =~ s/(?<=equisite).*?(\b[A-Za-z]{4}\d{4}\b)// );
+my $string = (`wget -q -O- $urlU` or `wget -q -O- $urlP`) 
+    or die "$0: Course not found: $course\n";
+print uc $1, "\n" while ($string =~ s/(?<=equisite)[^<]*?(\b[A-Za-z]{4}\d{4}\b)//);
