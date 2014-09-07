025a27c6cba842d0be258f25e8e30731bcd32fc3
CallumHoward <callumh@cse.unsw.edu.au>
2014-09-08 01:26:40 +1000

    regex tweak for prereq

--- week06/prereq.pl
+++ week06/prereq.pl
@@ -3,20 +3,23 @@
 use warnings;
 use strict;
 
-my $domain="www.handbook.unsw.edu.au";
-my $year=2014;
+my $domain = "www.handbook.unsw.edu.au";
+my $year = 2014;
 
-my $found=0;
+my $found = 0;
 my $content;
+$! = 1;
 
-my $course = $ARGV[0] or die "Usage: $0 <course-name>\n";
+my $course = $ARGV[0] or die "Usage: $0 <course-name>\n"; 
 $course =~ /^[A-Z]{4}\d{4}$/ or die "$0: Invalid course format: $course\n";
 
 foreach my $career ("undergraduate","postgraduate") {
     my $url = "http://$domain/$career/courses/$year/$course.html";
     $content .= `wget -q -O- $url`; 
-    $found += $?;
+    $found = ($found or not$?); $! = 1;
 }
 
 die "$0: Course not found: $course\n" if (!$found);
-print uc $1,"\n" while ($content =~ s/(?<=equisite)[^<]*?(\b[A-Za-z]{4}\d{4}\b)//);
+print uc $1,"\n" while ($content =~
+    s/(?<=equisite)[^<;]*?\b([A-Za-z]{4}\d{4})\b//);
+exit 0;
