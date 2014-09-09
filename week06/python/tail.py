24e80080337fdb37fe64320659d45b94a462a7e6
CallumHoward <callumh@cse.unsw.edu.au>
2014-09-09 14:28:32 +1000

    fixed for trailing newline

--- week06/python/tail.py
+++ week06/python/tail.py
@@ -10,6 +10,7 @@ for line in fileinput.input():
     files.setdefault(fileinput.filename(), []).append(line)
 
 for file in files.iterkeys():
+    files[file][-1] = files[file][-1].rstrip('\n')
     if len(files[file]) <= 10:
         print "".join(files[file])
     else:
