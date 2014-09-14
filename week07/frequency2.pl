cca840a9f69591877f8cdaa1c940c568a1d62f77
CallumHoward <callumh@cse.unsw.edu.au>
2014-09-14 18:58:00 +1000

    frequency.pl works

--- week07/frequency2.pl
+++ week07/frequency2.pl
@@ -6,7 +6,7 @@ use strict;
 for my $file (glob "poets/*.txt") {
     my ($c, $t, $s);
     open (F, "<", $file) or die "ERROR\n";
-    $t +=()= /[A-Za-z]+/g and $c +=()= /\b\Q$ARGV[0]\E\b/gi for <F>;
+    $c +=()= /\b\Q$ARGV[0]\E\b/gi; $t +=()= /\w+/g for <F>;
     $s = join("", $file =~ /[A-Z]\w+/g);
     $s =~ tr/_/ /;
     printf "%4d/%6d = %.9f %s\n", $c, $t, $c/$t, $s; 
