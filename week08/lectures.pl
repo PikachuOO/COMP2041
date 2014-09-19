d555e59d19215e60834737a345a1c94e5594d2a2
CallumHoward <callumh@cse.unsw.edu.au>
2014-09-19 22:23:21 +1000

    working with -d flag, but needs to be fixed

--- week08/lectures.pl
+++ week08/lectures.pl
@@ -3,11 +3,19 @@
 use warnings;
 use strict;
 
+my $formatFlag = shift @ARGV if $ARGV[0] =~ /-d/;
 for my $course (@ARGV) {
     my $url = "http://www.timetable.unsw.edu.au/current/$course.html";
     my $webcontent = `wget -q -O- $url`;
     my ($last, $curr) = 0;
     while ($webcontent =~ /Lecture[^<]*?<\/a>.*?([SX][12])(.*?<td class=){5}.*?>([^<]*?)</sg and $3) {
-        print $last = $curr if (($curr = "$course: $1 $3\n") ne $last);
+        $curr = "$course: $1 $3\n";
+        my $sem = $1;
+        if (defined $formatFlag and $curr ne $last) {
+            print "$sem $course $_\n" for $curr =~ /[MTWFS]\w\w\s\d\d(?=:)/g;
+        } else {
+            print $curr if $curr ne $last;
+        }
+        $last = $curr;
     }
 }
