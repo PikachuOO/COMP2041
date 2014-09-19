9c9cfce0eff312b9711fd57c7a2fb28093f0bd2f
CallumHoward <callumh@cse.unsw.edu.au>
2014-09-19 21:12:53 +1000

    duplicates broken again

--- week08/lectures.pl
+++ week08/lectures.pl
@@ -7,7 +7,8 @@ for my $course (@ARGV) {
     my $url = "http://www.timetable.unsw.edu.au/current/$course.html";
     my $webcontent = `wget -q -O- $url`;
     while ($webcontent =~
-        /(?=Lecture<\/a>).*?([SX][12])(.*?<td){5}.*?>([MTWFS]\w\w\b.+?)</sg) {
+        /Lecture[^<]*?<\/a>.*?([SX][12])(.*?<td class=){5}.*?>([^<]*?)</sg and
+        $3) {
         print "$course: $1 $3\n";
     }
 }
