b0c252c46d7f842edeef6c719932c012c5369e7c
CallumHoward <callumh@cse.unsw.edu.au>
2014-09-19 19:05:28 +1000

    renamed for dryrun

--- week08/lectures.pl
+++ week08/lectures.pl
@@ -3,16 +3,8 @@
 use warnings;
 use strict;
 
-my %lectures;
-
 for my $course (@ARGV) {
     my $url = "http://www.timetable.unsw.edu.au/current/$course.html";
     my $webcontent = `wget -q -O- $url`;
-    my @times = $webcontent =~ /(?=Lecture).*?([MTWFS]\w+\s+\d\d:\d\d\s-\s\d\d:\d\d\s\(\w+:\d-\d,\d\d-\d\d\),?\s*)+/sg;
-    for my $time (@times) {
-        print "$course: S? $time\n";
-    }
-
+    print "$course: S$1 $3\n" while $webcontent =~ /(?=Lecture).*?T([12])(.*?<td){5}.*?>(.*?)</sg and $3;
 }
-
-
