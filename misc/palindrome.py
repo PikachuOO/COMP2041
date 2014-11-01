aff776c2b1b03b120f2bb957c75db58ff25e44ac
CallumHoward <callumh@cse.unsw.edu.au>
2014-11-01 19:55:28 +1100

    interview practice questions

---
+++ misc/palindrome.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#
+# Calculate if string passed on STDIN is palindromic
+# Callum Howard, 2014
+
+import sys
+
+# read input
+testString = sys.raw_input()
+stringSize = testString.len()
+
+# strip spaces
+
+# half size of string and take floor
+
+# split at this index
+
+# iterate through both strings and check if one
